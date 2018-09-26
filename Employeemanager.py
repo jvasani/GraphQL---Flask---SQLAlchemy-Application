2aa0b4b647f1dca7157f88fbdcf491386a22c43a
Jigar Vasani <Mjigar64526@gmail.com>
2018-09-26 07:23:38 -0400

    All the Employees records Listed on the web page Local URL:  http://127.0.0.1:5000/employee

--- Employeemanager.py
+++ Employeemanager.py
@@ -1,11 +1,23 @@
 from flask import Flask
 from flask_graphql import GraphQLView
-
-from models import db_session
+from flask_sqlalchemy import  SQLAlchemy
+from models import db_session, table, Employee
 from schema import schema, Department
+from flask import render_template,request, redirect, flash
+from sqlalchemy import *
+
 
 app = Flask(__name__)
 app.debug = True
+db = SQLAlchemy(app)
+
+
+@app.route("/employee")
+def listAllEmployee():
+    employees = db_session.query(Employee).all();
+
+    table.border = True
+    return render_template("employeelist.html", myEmployee=employees)
 
 app.add_url_rule(
     '/graphql',
--- models.py
+++ models.py
@@ -1,14 +1,18 @@
 # flask_sqlalchemy/models.py
+
 from sqlalchemy import *
-from sqlalchemy.orm import (scoped_session, sessionmaker, relationship,
-                            backref)
+from sqlalchemy.orm import (scoped_session, sessionmaker, relationship, backref)
 from sqlalchemy.ext.declarative import declarative_base
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
 
 engine = create_engine("sqlite:///database.sqlite3", convert_unicode=True)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
                                          bind=engine))
-
+models = Flask("__name__")
+db = SQLAlchemy(models)
 Base = declarative_base()
 # We will need this for querying
 Base.query = db_session.query_property()
@@ -20,7 +24,17 @@ class Department(Base):
     name = Column(String)
 
 
-class Employee(Base):
+class Employee(db.Model):
+
     __tablename__ = 'employee'
     EmpID = Column(Integer, primary_key=True)
     EmployeeName = Column(String)
+    Department = Column(String)
+    JobTitle = Column(String)
+    Salary = Column(Integer)
+
+    def __init__(self, EmpID, EmployeeName, Department):
+        self.EmpID = EmpID
+        self.EmployeeName = EmployeeName
+        self.Department = Department
+
